8bbe252c6996c4123380d339316f8fc4b965f109
Michael Fairman <michael.fairman@chargepoint.com>
2020-03-10 19:57:30 -0700

    imx-gpu-viv: bump version to 6.2.4.p4.8
    This version works with the 4.14.98_2.3.0 kernel drivers.
    
    Signed-off-by: Michael Fairman <michael.fairman@chargepoint.com>

--- package/freescale-imx/imx-gpu-viv/imx-gpu-viv.mk
+++ package/freescale-imx/imx-gpu-viv/imx-gpu-viv.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 ifeq ($(BR2_aarch64),y)
-IMX_GPU_VIV_VERSION = 6.4.0.p1.0-aarch64
+IMX_GPU_VIV_VERSION = 6.2.4.p4.8-aarch64
 else
-IMX_GPU_VIV_VERSION = 6.4.0.p1.0-aarch32
+IMX_GPU_VIV_VERSION = 6.2.4.p4.8-aarch32
 endif
 IMX_GPU_VIV_SITE = $(FREESCALE_IMX_SITE)
 IMX_GPU_VIV_SOURCE = imx-gpu-viv-$(IMX_GPU_VIV_VERSION).bin
