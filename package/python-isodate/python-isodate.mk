2bb94a9c4a78ad227bb000d86966da1155356e4a
Trampus <trampus.richmond@chargepoint.com>
2020-06-23 11:24:15 -0700

    package: add finbox required packages for mainline
    I have added new packages to buildroot which are all public python
    pip projects that do not currently have an existing Buildroot package.
    These packages are needed for FinBox support.  Since all of the packages
    are just pip's and they can be useful to others I am adding them to be
    upstreamed later.  One of the packages is just a version bump needed
    for proper udev support.
    
    Since all of the packages are python, this should not affect any other
    builds.  Tested build using this branch as the submodule version in the
    existing FinBox build system.
    
    - Version bump on python-pyudev.
    - Added new packages to be upstreamed.
    
    Signed-off-by: Trampus <trampus.richmond@chargepoint.com>

---
+++ package/python-isodate/python-isodate.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-isodate
+#
+# https://github.com/gweis/isodate/archive/0.6.0.tar.gz
+#
+################################################################################
+
+PYTHON_ISODATE_VERSION = 0.6.0
+PYTHON_ISODATE_SOURCE = $(PYTHON_ISODATE_VERSION).tar.gz
+PYTHON_ISODATE_SITE = https://github.com/gweis/isodate/archive
+PYTHON_ISODATE_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
---
+++ package/python-labjack/python-labjack.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# python-labjack
+#
+#
+################################################################################
+
+PYTHON_LABJACK_VERSION = 2.0.0
+PYTHON_LABJACK_SOURCE = $(PYTHON_LABJACK_VERSION).tar.gz
+PYTHON_LABJACK_SITE = https://github.com/labjack/LabJackPython/archive
+PYTHON_LABJACK_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
