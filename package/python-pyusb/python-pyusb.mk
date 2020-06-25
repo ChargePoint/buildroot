8dd889a8381a2edabd57f30811120de9cd1687a4
Trampus <trampus.richmond@chargepoint.com>
2020-06-24 23:22:08 -0700

    package/python-jose: add new package python-jose version 3.0.1
    I have updated the version of one package, and added a new package.
    Both packages are python pips which are open source.  I tested the
    packages building BR as a submodule to FinBox.
    
    - Version bump on one package.
    - Added new package.
    
    Signed-off-by: Trampus <trampus.richmond@chargepoint.com>

--- package/python-pyusb/python-pyusb.mk
+++ package/python-pyusb/python-pyusb.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYUSB_VERSION = 1.0.0
-PYTHON_PYUSB_SOURCE = PyUSB-$(PYTHON_PYUSB_VERSION).tar.gz
-PYTHON_PYUSB_SITE = https://pypi.python.org/packages/8a/19/66fb48a4905e472f5dfeda3a1bafac369fbf6d6fc5cf55b780864962652d
+PYTHON_PYUSB_VERSION = 1.0.2
+PYTHON_PYUSB_SOURCE = pyusb-$(PYTHON_PYUSB_VERSION).tar.gz
+PYTHON_PYUSB_SITE = https://files.pythonhosted.org/packages/5f/34/2095e821c01225377dda4ebdbd53d8316d6abb243c9bee43d3888fa91dd6
 PYTHON_PYUSB_LICENSE = BSD-3-Clause
 PYTHON_PYUSB_LICENSE_FILES = LICENSE
 PYTHON_PYUSB_SETUP_TYPE = distutils
