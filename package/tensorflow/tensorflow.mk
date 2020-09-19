4b6d11f03dc500cc7cdd0b142b74939cc0069311
Sudhanshu Gupta <sudhanshu.gupta@chargepoint.com>
2020-09-19 08:31:12 +0530

    Adapting tensorflow install flow for remote downloads
    - Modified tensorflow.mk to download tarball from Tensorflow
        - VERSION points to tensorflow repo tag r2.3
    
    - Added tensorflow.hash for keeping hash of downloaded tarbar
        - Hash computed locally using sha256sum for downloaded tensorflow
        tar ball
    
    Signed-off-by: Sudhanshu Gupta <sudhanshu.gupta@chargepoint.com>

--- package/tensorflow/tensorflow.mk
+++ package/tensorflow/tensorflow.mk
@@ -3,53 +3,20 @@
 # tensorflow
 #
 ################################################################################
-TENSORFLOW_VERSION = 2.3
-#TENSORFLOW_SOURCE = libfoo-$(TENSORFLOW_VERSION).tar.gz
-#TENSORFLOW_SITE = http://www.foosoftware.org/download
-TENSORFLOW_LICENSE = GPL-3.0+
-TENSORFLOW_LICENSE_FILES = COPYING
+
+# CommitID  Tensor Flow version 2.3 - tag r2.3
+TENSORFLOW_VERSION = b36436b087bd8e8701ef51718179037cccdfc26e
+TENSORFLOW_SITE = $(call github,tensorflow,tensorflow,$(TENSORFLOW_VERSION))
+#TENSORFLOW_DEPENDENCIES = host-protobuf host-prototext \
+			       protobuf abseil flatbuffers eigen farmhash fft2d gtest \
+			       double-conversion re2 sqlite giflib snappy jpeg-turbo \
+			       libpng nsync grpc gemmlowp
+
+TENSORFLOW_LICENSE = Apache-2.0
+TENSORFLOW_LICENSE_FILES = LICENSE
+
 TENSORFLOW_INSTALL_STAGING = YES
-#TENSORFLOW_CONFIG_SCRIPTS = libfoo-config
-#TENSORFLOW_DEPENDENCIES = host-libaaa libbbb
 
-define TENSORFLOW_BUILD_CMDS
-	echo "-----------------------------------------Sudhanshu : Reached Tensorflow build command"
-	./tensorflow/lite/tools/make/download_dependencies.sh
-	./tensorflow/lite/tools/make/build_aarch64_lib.sh
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
-endef
+TENSORFLOW_CONF_OPTS += -DSYSROOT=$(STAGING_DIR)
 
-define TENSORFLOW_INSTALL_STAGING_CMDS
-
-	echo "-----------------------------------------Sudhanshu : Staging Cmds "
-	$(INSTALL) -D -m 0755 $(@D)/libfoo.a $(STAGING_DIR)/usr/lib/libfoo.a
-	$(INSTALL) -D -m 0644 $(@D)/foo.h $(STAGING_DIR)/usr/include/foo.h
-	$(INSTALL) -D -m 0755 $(@D)/libfoo.so* $(STAGING_DIR)/usr/lib
-endef
-
-define TENSORFLOW_INSTALL_TARGET_CMDS
-	echo "-----------------------------------------Sudhanshu : Install Cmds "
-	$(INSTALL) -D -m 0755 $(@D)/libfoo.so* $(TARGET_DIR)/usr/lib
-	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/foo.d
-endef
-
-$(eval $(generic-package))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(eval $(cmake-package))
