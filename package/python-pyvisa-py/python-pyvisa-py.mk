105a0cb456fd4f2988e4a72548178327f954ccac
Trampus Richmond <trampus.richmond@chargepoint.com>
2021-03-29 15:19:25 -0700

    Packages: Version bumps for needed finbox packages
    This is a version bump for a few packages which are needed to support
    finbox.  There is a patch file for setuptools, it is pretty much the
    same patch file as was there for the older version, just updated to
    patch this newer version.
    
    I have build and ran finbox with these changes, and everything seem to
    be working fine.
    
    - Version update for FinBox package deps.
    
    Signed-off-by: Trampus Richmond <trampus.richmond@chargepoint.com>

--- package/python-pyvisa-py/python-pyvisa-py.mk
+++ package/python-pyvisa-py/python-pyvisa-py.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-PYTHON_PYVISA_PY_VERSION = 0.4.0
-PYTHON_PYVISA_PY_SOURCE = $(PYTHON_PYVISA_PY_VERSION).tar.gz
-PYTHON_PYVISA_PY_SITE = https://github.com/pyvisa/pyvisa-py/archive
+PYTHON_PYVISA_PY_VERSION = 0.5.2
+PYTHON_PYVISA_PY_SOURCE = PyVISA-py-$(PYTHON_PYVISA_PY_VERSION).tar.gz
+PYTHON_PYVISA_PY_SITE = https://files.pythonhosted.org/packages/30/d2/67dc4b218db814fedaa616766d5ac999eb8771058573adee5f5038754ece
 PYTHON_PYVISA_PY_SETUP_TYPE = setuptools
+PYTHON_PYVISA_PY_DEPENDENCIES = host-python-setuptools-scm
 
 $(eval $(python-package))
