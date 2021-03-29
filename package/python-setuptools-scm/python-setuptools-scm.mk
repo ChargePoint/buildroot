b7466d284bdd347ac76fb167fd350714dbf69d1b
Trampus Richmond <trampus.richmond@chargepoint.com>
2021-03-29 15:19:25 -0700

    packages: version bumps for needed finbox packages
    This is a version bump for a few packages which are needed to support
    finbox.  There is a patch file for setuptools, it is pretty much the
    same patch file as was there for the older version, just updated to
    patch this newer version.
    
    I have build and ran finbox with these changes, and everything seem to
    be working fine.
    
    - Version update for FinBox package deps.
    
    Signed-off-by: Trampus Richmond <trampus.richmond@chargepoint.com>

--- package/python-setuptools-scm/python-setuptools-scm.mk
+++ package/python-setuptools-scm/python-setuptools-scm.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SETUPTOOLS_SCM_VERSION = 3.5.0
+PYTHON_SETUPTOOLS_SCM_VERSION = 6.0.1
 PYTHON_SETUPTOOLS_SCM_SOURCE = setuptools_scm-$(PYTHON_SETUPTOOLS_SCM_VERSION).tar.gz
-PYTHON_SETUPTOOLS_SCM_SITE = https://files.pythonhosted.org/packages/b2/f7/60a645aae001a2e06cf4b8db2fba9d9f36b8fd378f10647e3e218b61b74b
+PYTHON_SETUPTOOLS_SCM_SITE = https://files.pythonhosted.org/packages/57/38/930b1241372a9f266a7df2b184fb9d4f497c2cef2e016b014f82f541fe7c
 PYTHON_SETUPTOOLS_SCM_LICENSE = MIT
 PYTHON_SETUPTOOLS_SCM_LICENSE_FILES = LICENSE
 PYTHON_SETUPTOOLS_SCM_SETUP_TYPE = setuptools
--- package/python-visa/python-visa.mk
+++ package/python-visa/python-visa.mk
@@ -5,9 +5,9 @@
 #
 ################################################################################
 
-PYTHON_VISA_VERSION = 1.10.1
+PYTHON_VISA_VERSION = 1.11.3
 PYTHON_VISA_SOURCE = PyVISA-$(PYTHON_VISA_VERSION).tar.gz
-PYTHON_VISA_SITE = https://files.pythonhosted.org/packages/51/d1/fc21b45228718cde06b33b08761a6a9aa859dfe51dfbf07e81e0f209e776
+PYTHON_VISA_SITE = https://files.pythonhosted.org/packages/82/d1/9d19b5bee77eb030425028ab8b53880aae859b1f88b7981edffe3818ee3d
 PYTHON_VISA_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
