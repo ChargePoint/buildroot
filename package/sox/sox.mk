e100e3425e917376eaa1d9142c177ea49003b959
mhungerford-cpi <matt.hungerford@chargepoint.com>
2021-04-28 13:52:59 -0700

    Packages: sox - makefile update
    Fix sox build to install files required by cmake into toolchain.

--- package/sox/sox.mk
+++ package/sox/sox.mk
@@ -10,6 +10,7 @@ SOX_SOURCE = sox-$(SOX_VERSION).tar.bz2
 SOX_DEPENDENCIES = host-pkgconf
 SOX_LICENSE = GPL-2.0+ (sox binary), LGPL-2.1+ (libraries)
 SOX_LICENSE_FILES = LICENSE.GPL LICENSE.LGPL
+SOX_INSTALL_STAGING = YES
 
 SOX_CONF_OPTS = \
 	--with-distro="Buildroot" \
