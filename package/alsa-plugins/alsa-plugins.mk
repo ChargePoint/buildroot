256bffe8bf8e97751974d7c03d4c10587e2b6fb5
Charles Hardin <charles.hardin@chargepoint.com>
2021-06-25 15:53:56 -0700

    package/alsa-plugins: fix the dependencies against alsa-utils
    The alsa-utils will remove and create the /usr/share/alsa dir
    as a whole and since the alsa plugins needs to add config into
    this same directory it should come after those install steps.
    
    Signed-off-by: Charles Hardin <charles.hardin@chargepoint.com>

--- package/alsa-plugins/alsa-plugins.mk
+++ package/alsa-plugins/alsa-plugins.mk
@@ -7,26 +7,21 @@
 ALSA_PLUGINS_VERSION = 1.2.7.1
 ALSA_PLUGINS_SOURCE = alsa-plugins-$(ALSA_PLUGINS_VERSION).tar.bz2
 ALSA_PLUGINS_SITE = https://www.alsa-project.org/files/pub/plugins
-ALSA_PLUGINS_LICENSE = LGPL-2.1+
-ALSA_PLUGINS_LICENSE_FILES = COPYING
-ALSA_PLUGINS_DEPENDENCIES = host-pkgconf alsa-lib
+ALSA_PLUGINS_LICENSE = LGPL-2.1+ (library), GPL-2.0+ (samplerate)
+ALSA_PLUGINS_LICENSE_FILES = COPYING COPYING.GPL
+ALSA_PLUGINS_AUTORECONF = YES
+ALSA_PLUGINS_DEPENDENCIES = host-pkgconf alsa-lib libsamplerate
 
-ALSA_PLUGINS_CONF_OPTS = \
+ifeq ($(BR2_PACKAGE_ALSA_UTILS),y)
+ALSA_PLUGINS_DEPENDENCIES += alsa-utils
+endif
+
+ALSA_PLUGINS_CONF_OPTS += \
+	--enable-samplerate \
 	--disable-jack \
 	--disable-usbstream \
 	--disable-pulseaudio \
-	--disable-libav \
-	--disable-maemo-plugin \
-	--disable-maemo-resource-manager \
-	--with-speex=no
-
-ifeq ($(BR2_PACKAGE_LIBSAMPLERATE),y)
-ALSA_PLUGINS_CONF_OPTS += --enable-samplerate
-ALSA_PLUGINS_DEPENDENCIES += libsamplerate
-ALSA_PLUGINS_LICENSE += , GPL-2.0+ (samplerate plugin)
-ALSA_PLUGINS_LICENSE_FILES += COPYING.GPL
-else
-ALSA_PLUGINS_CONF_OPTS += --disable-samplerate
-endif
+	--disable-avcodec \
+	--with-speex=builtin
 
 $(eval $(autotools-package))
