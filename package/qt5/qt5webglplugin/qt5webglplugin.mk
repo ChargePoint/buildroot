b17e855d6c385225cbe4b3ed5b05e7a2828767db
mhungerford-cpi <matt.hungerford@chargepoint.com>
2021-06-30 14:57:32 -0700

    package/qt5: add qtwebglplugin to qt5 package for remote UI access
    Qt provides several plugins for graphics rendering.  Adding the webgl
    plugin to buildroot qt5 package.
    
    Signed-off-by: Matthew Hungerford <mhungerford@gmail.com>

---
+++ package/qt5/qt5webglplugin/qt5webglplugin.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# qt5webglplugin
+#
+################################################################################
+
+QT5WEBGLPLUGIN_VERSION = $(QT5_VERSION)
+QT5WEBGLPLUGIN_SITE = $(QT5_SITE)
+QT5WEBGLPLUGIN_SOURCE = qtwebglplugin-$(QT5_SOURCE_TARBALL_PREFIX)-$(QT5WEBGLPLUGIN_VERSION).tar.xz
+QT5WEBGLPLUGIN_DEPENDENCIES = qt5declarative qt5websockets
+QT5WEBGLPLUGIN_INSTALL_STAGING = NO
+
+QT5WEBGLPLUGIN_LICENSE = GPL-3.0
+QT5WEBGLPLUGIN_LICENSE_FILES = LICENSE.GPL3
+
+$(eval $(qmake-package))
