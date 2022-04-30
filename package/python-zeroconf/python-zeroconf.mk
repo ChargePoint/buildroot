d1f4f1ad82087564f7a601987be7afd88f0a370a
Jeffrey Hart <jeffrey.hart@chargepoint.com>
2022-04-29 20:03:48 -0700

    python-zeroconf: update to 0.38.4
    Update python-zeroconf to 0.38.4.
    
    Signed-off-by: Jeffrey Hart <jeffrey.hart@chargepoint.com>

--- package/python-zeroconf/python-zeroconf.mk
+++ package/python-zeroconf/python-zeroconf.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_ZEROCONF_VERSION = 0.29.0
+PYTHON_ZEROCONF_VERSION = 0.38.4
 PYTHON_ZEROCONF_SOURCE = zeroconf-$(PYTHON_ZEROCONF_VERSION).tar.gz
-PYTHON_ZEROCONF_SITE = https://files.pythonhosted.org/packages/ca/77/eb6137997adc60811c6c46b28b00abac5c16daf14383f61d8a0180326b38
+PYTHON_ZEROCONF_SITE = https://files.pythonhosted.org/packages/a9/af/d5a16dc6081dc41507b65ed0d499e3c71728a3fbec4c14df6b2ec8eae1a7
 PYTHON_ZEROCONF_SETUP_TYPE = setuptools
 PYTHON_ZEROCONF_LICENSE = LGPL-2.1+
 PYTHON_ZEROCONF_LICENSE_FILES = COPYING
