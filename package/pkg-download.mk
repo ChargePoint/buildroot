f55666ba039a218015d8dbfca77c03d9cc94e948
Charles Hardin <charles.hardin@chargepoint.com>
2023-06-20 19:03:14 -0700

    support/download: add a helper scipt to generate scmversions
    Primarily focused on uboot and linux, getting the scmversion from
    the custom repository references is required for change control
    tracking off of different builds and pipelines. So, extend the
    download framework to generate these files while the locks are
    being held for the generation to avoid the git information from
    changes during the download process.
    
    Signed-off-by: Charles Hardin <charles.hardin@chargepoint.com>

--- package/pkg-download.mk
+++ package/pkg-download.mk
@@ -119,6 +119,7 @@ define DOWNLOAD
 		-N '$($(2)_RAWNAME)' \
 		-o '$($(2)_DL_DIR)/$(notdir $(1))' \
 		$(if $(filter YES,$($(2)_SVN_EXTERNALS)),-r) \
+		$(if $($(2)_SCMVERSION),-s) \
 		$(if $($(2)_GIT_SUBMODULES),-r) \
 		$(if $($(2)_GIT_LFS),-l) \
 		$(foreach uri,$(call DOWNLOAD_URIS,$(1),$(2)),-u $(uri)) \
